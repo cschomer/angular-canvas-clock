18fd0ec2e743b91007a656d86f7101544bb6030c
coreyschomer <coreyschomer@gmail.com>
2018-02-03 17:49:49 -0700

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cs-root',
+  template: `<cs-canvas-clock></cs-canvas-clock>`,
+  styles: []
+})
+export class AppComponent {
+  title = '';
+}
