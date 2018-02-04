9ed89829eeb7cdab488a08ddfcb2562ed666bac8
coreyschomer <coreyschomer@gmail.com>
2018-02-03 18:17:08 -0700

    add title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cs-root',
-  template: `Stackblitz = Awesome <cs-canvas-clock></cs-canvas-clock>`,
-  styles: []
+  template: `<h1>{{ title }}</h1> <h1>{{ subtitle }}</h1> <cs-canvas-clock></cs-canvas-clock>`,
+  styles: [`:host { display: grid; justify-items: center; }`]
 })
 export class AppComponent {
-  title = '';
+  title = 'Angular Canvas Clock';
+  subtitle = 'Stackblitz = Awesome';
 }
